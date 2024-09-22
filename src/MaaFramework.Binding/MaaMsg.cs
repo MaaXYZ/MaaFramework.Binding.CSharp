20ff293fa3d2ea62543b3159d65e17edf8ae9e53
枫雨 <35213527+moomiji@users.noreply.github.com>
2024-09-22 19:43:04 +0800

    chore: update interop to framework 2.0.0

--- src/MaaFramework.Binding/MaaMsg.cs
+++ src/MaaFramework.Binding/MaaMsg.cs
@@ -12,7 +12,7 @@
 
 ﻿namespace MaaFramework.Binding.Messages;
 
-//MaaApiDocument Version: (main) v2.0.0-beta.4-post.2-ci.10955639000
+//MaaApiDocument Version: (main) v2.0.0
 /// <summary>
 ///  A callback consists of a message and a payload.
 ///  The message is a string that indicates the type of the message.
