98845e1e5bc0a99b3c553133b9e37a8acedc5613
moomiji <35213527+moomiji@users.noreply.github.com>
2024-10-26 23:53:10 +0800

    feat: MaaTaskJob MaaTaskJob.WaitFor()

--- src/MaaFramework.Binding/MaaTaskJob.cs
+++ src/MaaFramework.Binding/MaaTaskJob.cs
@@ -13,4 +13,12 @@ public class MaaTaskJob(MaaId id, IMaaTasker tasker) : MaaJob(id, tasker)
     ///     A property used to simplify design of <see cref="TaskDetail.Query"/>.
     /// </remarks>
     public IMaaTasker Tasker => tasker;
+
+    /// <summary>
+    ///     Waits for a <see cref="MaaTaskJob"/> to complete with an expected <paramref name="status"/>.
+    /// </summary>
+    /// <param name="status">The expected status.</param>
+    /// <returns><see langword="this"/> if it completes with the <paramref name="status"/>; otherwise, <see langword="null"/>.</returns>
+    public MaaTaskJob? WaitFor(MaaJobStatus status)
+        => Wait() == status ? this : null;
 }
