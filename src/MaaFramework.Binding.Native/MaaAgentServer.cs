f441a6a77dc626a269c5be1a1f18ac75cd9a01e6
moomiji <35213527+moomiji@users.noreply.github.com>
2025-03-21 18:15:12 +0800

    feat: add wrapper of MaaAgent

---
+++ src/MaaFramework.Binding.Native/MaaAgentServer.cs
@@ -0,0 +1,53 @@
+﻿using MaaFramework.Binding.Custom;
+using MaaFramework.Binding.Interop.Native;
+using static MaaFramework.Binding.Interop.Native.MaaAgentServer;
+
+namespace MaaFramework.Binding.Native;
+
+/// <summary>
+///     A wrapper class providing a reference implementation for <see cref="MaaFramework.Binding.Interop.Native.MaaAgentServer"/>.
+/// </summary>
+public class MaaAgentServer : IMaaAgentServer
+{
+    private readonly MaaMarshaledApiRegistry<MaaCustomActionCallback> _actions = new();
+    private readonly MaaMarshaledApiRegistry<MaaCustomRecognitionCallback> _recognitions = new();
+
+    /// <inheritdoc/>
+    public bool Register<T>(string name, T custom) where T : IMaaCustomResource
+    {
+        custom.Name = name;
+        return Register(custom);
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentServerRegisterCustomAction"/> and <see cref="MaaAgentServerRegisterCustomRecognition"/>.
+    /// </remarks>
+    public bool Register<T>(T custom) where T : IMaaCustomResource => custom switch
+    {
+        IMaaCustomAction res
+            => MaaAgentServerRegisterCustomAction(res.Name, res.Convert(out var callback), nint.Zero)
+            && _actions.Register(res.Name, callback),
+        IMaaCustomRecognition res
+            => MaaAgentServerRegisterCustomRecognition(res.Name, res.Convert(out var callback), nint.Zero)
+            && _recognitions.Register(res.Name, callback),
+        _
+            => throw new NotImplementedException($"Type '{typeof(T)}' is not implemented."),
+    };
+
+    /// <inheritdoc/>
+    public bool StartUp(string identifier)
+        => MaaAgentServerStartUp(identifier);
+
+    /// <inheritdoc/>
+    public void ShutDown()
+        => MaaAgentServerShutDown();
+
+    /// <inheritdoc/>
+    public void Join()
+        => MaaAgentServerJoin();
+
+    /// <inheritdoc/>
+    public void Detach()
+        => MaaAgentServerDetach();
+}
---
+++ src/MaaFramework.Binding/IMaaAgentServer.cs
@@ -0,0 +1,43 @@
+﻿using MaaFramework.Binding.Custom;
+
+namespace MaaFramework.Binding;
+
+/// <summary>
+///     An interface defining wrapped members for MaaAgentServer.
+/// </summary>
+public interface IMaaAgentServer
+{
+    /// <summary>
+    ///     Registers a <see cref="IMaaCustomAction"/> or <see cref="IMaaCustomRecognition"/> in the <see cref="IMaaAgentServer"/>.
+    /// </summary>
+    /// <typeparam name="T">The <see cref="IMaaCustomAction"/> or <see cref="IMaaCustomRecognition"/>.</typeparam>
+    /// <param name="name">The new name that will be used to reference the custom resource.</param>
+    /// <param name="custom">The custom resource instance to register.</param>
+    /// <returns><see langword="true"/> if the registration was successful; otherwise, <see langword="false"/>.</returns>
+    bool Register<T>(string name, T custom) where T : IMaaCustomResource;
+
+    /// <inheritdoc cref="Register{T}(string, T)"/>
+    bool Register<T>(T custom) where T : IMaaCustomResource;
+
+    /// <summary>
+    ///     Starts up the agent server to prepare for receiving client messages from the specified connection.
+    /// </summary>
+    /// <param name="identifier">The connection identifier.</param>
+    /// <returns><see langword="true"/> if the server started successfully; otherwise, <see langword="false"/>.</returns>
+    bool StartUp(string identifier);
+
+    /// <summary>
+    ///     Shuts down the agent server.
+    /// </summary>
+    void ShutDown();
+
+    /// <summary>
+    ///     Blocks the calling thread until the thread for receiving client messages finishes its execution.
+    /// </summary>
+    void Join();
+
+    /// <summary>
+    ///     Separates the thread for receiving client messages, allowing execution to continue independently.
+    /// </summary>
+    void Detach();
+}
