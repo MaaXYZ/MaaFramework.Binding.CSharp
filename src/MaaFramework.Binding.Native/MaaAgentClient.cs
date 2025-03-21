0682bd944e3ff5ed18c0bc8984ecf9746afa40f5
moomiji <35213527+moomiji@users.noreply.github.com>
2025-03-21 18:15:12 +0800

    feat: add wrapper of MaaAgent

---
+++ src/MaaFramework.Binding.Native/MaaAgentClient.cs
@@ -0,0 +1,109 @@
+﻿using MaaFramework.Binding.Abstractions;
+using MaaFramework.Binding.Interop.Native;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using static MaaFramework.Binding.Interop.Native.MaaAgentClient;
+using static MaaFramework.Binding.Interop.Native.MaaBuffer;
+
+namespace MaaFramework.Binding;
+
+/// <summary>
+///     A wrapper class providing a reference implementation for <see cref="MaaFramework.Binding.Interop.Native.MaaAgentClient"/>.
+/// </summary>
+[DebuggerDisplay("{DebuggerDisplay,nq}")]
+public class MaaAgentClient : MaaDisposableHandle<MaaAgentClientHandle>, IMaaAgentClient<MaaAgentClientHandle>
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)] private string DebuggerDisplay => $"{{{GetType().Name} {{ Disposed = {IsInvalid} }}}}";
+
+    /// <summary>
+    ///     Creates a <see cref="MaaAgentClient"/> instance.
+    /// </summary>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientCreate"/>.
+    /// </remarks>
+    public MaaAgentClient()
+        : base(invalidHandleValue: MaaAgentClientHandle.Zero)
+    {
+        var handle = MaaAgentClientCreate();
+        SetHandle(handle, needReleased: true);
+    }
+
+    /// <param name="resource">The resource.</param>
+    /// <param name="disposeOptions">The dispose options.</param>
+    /// <inheritdoc cref="MaaAgentClient()"/>
+    [SetsRequiredMembers]
+    public MaaAgentClient(MaaResource resource, DisposeOptions disposeOptions)
+        : this()
+    {
+        Resource = resource;
+        DisposeOptions = disposeOptions;
+    }
+
+    /// <inheritdoc/>
+    public required DisposeOptions DisposeOptions { get; set; }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientDestroy"/>.
+    /// </remarks>
+    protected override void ReleaseHandle()
+    {
+        // Cannot destroy Instance before disposing Resource.
+
+        if (DisposeOptions.HasFlag(DisposeOptions.Resource))
+        {
+            Resource.Dispose();
+        }
+
+        MaaAgentClientDestroy(Handle);
+    }
+
+    /// <inheritdoc/>
+    IMaaResource IMaaAgentClient.Resource
+    {
+        get => Resource;
+        set => Resource = (MaaResource)value;
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientBindResource"/>.
+    /// </remarks>
+    public required MaaResource Resource
+    {
+        get => field;
+        set
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _ = MaaAgentClientBindResource(Handle, value.Handle).ThrowIfFalse(MaaInteroperationException.ResourceBindingFailedMessage);
+            field = value;
+        }
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientCreateSocket"/>.
+    /// </remarks>
+    public bool CreateSocket(string identifier)
+    {
+        var handle = MaaStringBufferCreate();
+        var ret = MaaStringBufferSetEx(Handle, identifier) && MaaAgentClientCreateSocket(Handle, handle);
+        MaaStringBufferDestroy(handle);
+        return ret;
+    }
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientConnect"/>.
+    /// </remarks>
+    public bool LinkStart()
+        => MaaAgentClientConnect(Handle);
+
+    /// <inheritdoc/>
+    /// <remarks>
+    ///     Wrapper of <see cref="MaaAgentClientDisconnect"/>.
+    /// </remarks>
+    public bool LinkStop()
+        => MaaAgentClientDisconnect(Handle);
+}
+
---
+++ src/MaaFramework.Binding/IMaaAgentClient.cs
@@ -0,0 +1,46 @@
+﻿using MaaFramework.Binding.Abstractions;
+
+namespace MaaFramework.Binding;
+
+/// <summary>
+///     An interface defining wrapped members for MaaAgentClient with generic handle.
+/// </summary>
+/// <typeparam name="T">The type of handle.</typeparam>
+public interface IMaaAgentClient<out T> : IMaaAgentClient, IMaaDisposableHandle<T>;
+
+/// <summary>
+///     An interface defining wrapped members for MaaAgentClient.
+/// </summary>
+public interface IMaaAgentClient : IMaaDisposable
+{
+    /// <summary>
+    ///     Gets or sets whether disposes the <see cref="Resource"/> when <see cref="IDisposable.Dispose"/> was invoked.
+    /// </summary>
+    DisposeOptions DisposeOptions { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a resource that binds to the <see cref="IMaaAgentClient"/>.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"/>
+    /// <exception cref="MaaInteroperationException"/>
+    IMaaResource Resource { get; set; }
+
+    /// <summary>
+    ///     Creates a socket connection with the specified identifier.
+    /// </summary>
+    /// <param name="identifier">The connection identifier.</param>
+    /// <returns><see langword="true"/> if the socket was created successfully; otherwise, <see langword="false"/>.</returns>
+    bool CreateSocket(string identifier);
+
+    /// <summary>
+    ///     Starts the connection.
+    /// </summary>
+    /// <returns><see langword="true"/> if the connection was started successfully; otherwise, <see langword="false"/>.</returns>
+    bool LinkStart();
+
+    /// <summary>
+    ///     Stops the connection.
+    /// </summary>
+    /// <returns><see langword="true"/> if the connection was stopped successfully; otherwise, <see langword="false"/>.</returns>
+    bool LinkStop();
+}
