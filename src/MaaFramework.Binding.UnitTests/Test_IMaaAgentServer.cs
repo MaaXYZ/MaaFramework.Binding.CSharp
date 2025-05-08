25efb4bc9223c2cf41baddcdb9ce770881626984
moomiji <35213527+moomiji@users.noreply.github.com>
2025-05-08 16:45:15 +0800

    refactor: MaaAgentServer

--- src/MaaFramework.Binding.UnitTests/Test_IMaaAgentServer.cs
+++ src/MaaFramework.Binding.UnitTests/Test_IMaaAgentServer.cs
@@ -14,15 +14,21 @@ internal static class Test_IMaaAgentServer
         var socketId = commandLineArgs[^1];
         var userPath = commandLineArgs[^2];
         var dllPath = commandLineArgs[^3];
+        Test(socketId, userPath, dllPath);
 
-        NativeBindingInfo.Set(isAgentServer: true, dllPath); // First step
-        _ = new MaaToolkit(true, userPath);
-        var agentServer = new MaaAgentServer();
-        _ = agentServer.Register(Custom.Recognition);
-        _ = agentServer.Register(Custom.Action);
-        _ = agentServer.StartUp(socketId);
-        agentServer.Join();
-        agentServer.ShutDown();
         return 0;
+    }
+
+    public static void Test(string id, string userPath, string dllPath)
+    {
+        _ = MaaAgentServer.Current // test double call
+            .WithIdentifier(id).WithIdentifier(id)
+            .WithNativeLibrary(dllPath).WithNativeLibrary(dllPath)
+            .WithToolkitConfig_InitOption(userPath).WithToolkitConfig_InitOption(userPath)
+            .Register(Custom.Recognition) // cat not double call from here
+            .Register(Custom.Action)
+            .StartUp()
+            .Join().Join() // test double call
+            .ShutDown().ShutDown();
     }
 }
