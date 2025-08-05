57255c31a1bfe1016ff1e59a2a4bb274c7f42eb8
moomiji <35213527+moomiji@users.noreply.github.com>
2025-08-05 22:33:51 +0800

    perf: avoid bytes GC in MaaStringBufferSetEx()

--- src/MaaFramework.Binding.Native/Interop/MaaExtraInterop.cs
+++ src/MaaFramework.Binding.Native/Interop/MaaExtraInterop.cs
@@ -10,10 +10,50 @@ namespace MaaFramework.Binding.Interop.Native;
 
 public static partial class MaaBuffer
 {
-    public static bool MaaStringBufferSetEx(MaaStringBufferHandle handle, string str)
+    public static unsafe bool MaaStringBufferSetEx(MaaStringBufferHandle handle, string str)
     {
-        var bytes = Encoding.UTF8.GetBytes(str);
-        return MaaStringBufferSetEx(handle, bytes, (MaaSize)bytes.LongLength);
+        if (str is null)
+        {
+            return MaaStringBufferSetExFromNint(handle, nint.Zero, size: 0);
+        }
+
+        var allocated = false;
+        void* unmanagedValuePtr = null;
+        try
+        {
+            const long MaxUtf8BytesPerChar = 3;
+            var charCount = str.Length;
+            var bufferSize = 0x100;
+            var buffer = stackalloc byte[bufferSize];
+
+            // > for no null terminator
+            // Use the cast to long to avoid the checked operation
+            if (MaxUtf8BytesPerChar * charCount > bufferSize)
+            {
+                // Calculate accurate byte count when the provided stack-allocated buffer is not sufficient
+                var exactByteCount = Encoding.UTF8.GetByteCount(str);
+                if (exactByteCount > bufferSize)
+                {
+                    allocated = true;
+                    unmanagedValuePtr = NativeMemory.Alloc((nuint)exactByteCount);
+                    buffer = (byte*)unmanagedValuePtr;
+                    bufferSize = exactByteCount;
+                }
+            }
+
+            fixed (char* pChar = str)
+            {
+                var byteCount = Encoding.UTF8.GetBytes(pChar, charCount, buffer, bufferSize);
+                return MaaStringBufferSetExFromNint(handle, (nint)buffer, (MaaSize)byteCount);
+            }
+        }
+        finally
+        {
+            if (allocated)
+            {
+                NativeMemory.Free(unmanagedValuePtr);
+            }
+        }
     }
 
     [LibraryImport("MaaFramework", EntryPoint = "MaaStringBufferSetEx", StringMarshalling = StringMarshalling.Utf8)]
