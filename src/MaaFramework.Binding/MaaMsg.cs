dd913dc12310c774675c04c8fe61c4be17c0df3a
moomiji <35213527+moomiji@users.noreply.github.com>
2025-12-05 01:50:20 +0800

    chore: update interop to framework v5.1.0
    #20

--- src/MaaFramework.Binding/MaaMsg.cs
+++ src/MaaFramework.Binding/MaaMsg.cs
@@ -12,7 +12,7 @@
 
 namespace MaaFramework.Binding.Notification;
 
-//MaaApiDocument _version: (main) v5.1.0
+//MaaApiDocument _version: (main) v5.1.4
 /// <summary>
 ///  A callback consists of a message and a payload.
 ///  The message is a string that indicates the type of the message.
